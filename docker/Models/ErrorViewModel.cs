d04ac80c1ffb89bbc69123c1e1d250a8cd0c8d2a
novelkhan <novel4004@gmail.com>
2022-07-01 05:13:39 +0600

    Add project files.

---
+++ docker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
